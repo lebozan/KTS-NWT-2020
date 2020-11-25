befadd38fc34dad89548df6e7605098ba2e21836
Bojan Cakić <cakicbojan@gmail.com>
2020-11-25 18:40:28 +0100

    Project initialization

---
+++ Cultural-content-app/src/main/java/com/ktsnwt/Culturalcontentapp/CulturalContentAppApplication.java
@@ -0,0 +1,13 @@
+package com.ktsnwt.Culturalcontentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalContentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalContentAppApplication.class, args);
+	}
+
+}
---
+++ Cultural-content-app/src/test/java/com/ktsnwt/Culturalcontentapp/CulturalContentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktsnwt.Culturalcontentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalContentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
